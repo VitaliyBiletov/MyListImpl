99f0265025992c1387e91471e203e52de3664a82
Vitaliy Biletov <vit.biletov@gmail.com>
2020-02-19 13:43:08 +0300

    add method Sum

--- ListImpl.java
+++ ListImpl.java
@@ -1,3 +1,14 @@
-class ListImpl implements List{
-	int i = 0;
+class ListImpl{
+	private int x;
+	private int y;
+
+	public ListImpl(final int x, final int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public int sum(){
+		return this.x + this.y;
+	}
+
 }
